c4edebf60b09cdf91b4a3835f9d4d84f57cab29b
Tamas Toth <49683313+ttoth76@users.noreply.github.com>
2022-07-20 14:23:44 -0500

    Update server.R

--- server.R
+++ server.R
@@ -1,11 +1,5 @@
 #
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+# This is the server logic of a Shiny web application.
 
 library(shiny)
 
